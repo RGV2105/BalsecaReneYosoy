0400356d179338ff0eeda51847d24fa257bb66ed
RGV2105 <carlos.balseca.nevarez@utelvt.edu.ec>
2022-06-02 21:31:39 -0500

    subida de archivos de un menu de suma y compara

--- menu.cpp
+++ menu.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
+
 using namespace std;
 int main(){
 
-	int op;
+	int op,flag;
 	do{
-	cout<<"uma de varios numeros"<<endl;
-	cout<<"compara"<<endl;
-	cout<<"salir"<<endl;
+		system("clear");
+	cout<<"uma de varios numeros /n";
+	cout<<"compara /n";
+	cout<<"salir/n";
 	cin>>op;
+
+	switch(op){
+		case 1:
+			cout<<"aqui sesuma varios numeros ";
+			flag=cin.ignore().get();
+			break;
+		case 2:
+			cout<<"aqui se compara losnumeros ";
+			flag=cin.ignore().get();
+			break;
+		default:
+			cout<<"opcion no computable ";
+			flag=cin.ignore().get();
+
+	}
+
 	}while(op !=3);
 
 return 0;
