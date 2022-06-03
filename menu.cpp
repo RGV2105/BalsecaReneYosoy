021655f21568251b2a07aadb7791baa0d70091f5
RGV2105 <carlos.balseca.nevarez@utelvt.edu.ec>
2022-06-02 21:05:19 -0500

    subida de archivos de un menu de suma y compara

---
+++ menu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	int op;
+	do{
+	cout<<"uma de varios numeros"<<endl;
+	cout<<"compara"<<endl;
+	cout<<"salir"<<endl;
+	cin>>op;
+	}while(op !=3);
+
+return 0;
+}
